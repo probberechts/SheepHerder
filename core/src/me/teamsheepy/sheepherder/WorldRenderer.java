82807e8cdf01832d64c2caa36e5b17cb386a5407
Pieter Robberechts <pieter_robberechts@msn.com>
2014-10-29 18:19:11 +0100

    Aanpassingen van Tom naar development branch verplaatst

--- core/src/me/teamsheepy/sheepherder/WorldRenderer.java
+++ core/src/me/teamsheepy/sheepherder/WorldRenderer.java
@@ -6,8 +6,12 @@ import me.teamsheepy.sheepherder.objects.Sheep;
 import me.teamsheepy.sheepherder.objects.Tree;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.glutils.ImmediateModeRenderer;
+import com.badlogic.gdx.graphics.glutils.ImmediateModeRenderer20;
+import com.badlogic.gdx.math.Vector2;
 
 public class WorldRenderer {
 	
@@ -15,10 +19,12 @@ public class WorldRenderer {
 	static final float SCREEN_HEIGHT = 800;
 	private World world;
 	private SpriteBatch batch;
+	ImmediateModeRenderer immediateRenderer;
 
 	public WorldRenderer (SpriteBatch batch, World world) {
 		this.world = world;
 		this.batch = batch;
+		this.immediateRenderer = new ImmediateModeRenderer20(false, true, 0);
 	}
 
 	public void render () {
@@ -29,6 +35,29 @@ public class WorldRenderer {
 	public void renderBackground () {
 		Gdx.gl.glClearColor(0.172f, 0.690f, 0.212f, 1);
 	    Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+	    renderRect(new Vector2(0, 0), new Vector2(World.WORLD_MARGIN, World.WORLD_HEIGHT), World.WORLD_MARGIN_COLOR);
+	    renderRect(new Vector2(World.WORLD_WIDTH-World.WORLD_MARGIN, 0), new Vector2(World.WORLD_WIDTH, World.WORLD_HEIGHT), World.WORLD_MARGIN_COLOR);
+	    renderRect(new Vector2(0, 0), new Vector2(World.WORLD_WIDTH, World.WORLD_MARGIN), World.WORLD_MARGIN_COLOR);
+	    renderRect(new Vector2(0, World.WORLD_HEIGHT-World.WORLD_MARGIN), new Vector2(World.WORLD_WIDTH, World.WORLD_HEIGHT), World.WORLD_MARGIN_COLOR);
+	}
+
+	//bottom left & top right coords
+	private void renderRect(Vector2 p1, Vector2 p2, Color color){
+		immediateRenderer.begin(batch.getProjectionMatrix(), GL20.GL_TRIANGLE_STRIP);
+		immediateRenderer.color(color);
+	    immediateRenderer.vertex(p1.x, p1.y, 0);
+	    immediateRenderer.color(color);
+	    immediateRenderer.vertex(p2.x, p1.y, 0);
+	    immediateRenderer.color(color);
+	    immediateRenderer.vertex(p2.x, p2.y, 0);
+	    immediateRenderer.color(color);
+	    immediateRenderer.vertex(p2.x, p2.y, 0);
+	    immediateRenderer.color(color);
+	    immediateRenderer.vertex(p1.x, p2.y, 0);
+	    immediateRenderer.color(color);
+	    immediateRenderer.vertex(p1.x, p1.y, 0);
+	    immediateRenderer.end();
 	}
 	
 	public void renderObjects () {
