238d315e88d84125d4c1192ea5a4c2f7a1fb5643
Pieter Robberechts <pieter_robberechts@msn.com>
2014-11-07 15:54:54 +0100

    aanpassing om te kunnen publishen

--- desktop/src/me/teamsheepy/sheepherder/desktop/DesktopLauncher.java
+++ desktop/src/me/teamsheepy/sheepherder/desktop/DesktopLauncher.java
@@ -4,26 +4,23 @@ import me.teamsheepy.sheepherder.SheepHerder;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-import com.badlogic.gdx.graphics.Texture.TextureFilter;
-import com.badlogic.gdx.tools.texturepacker.TexturePacker;
-import com.badlogic.gdx.tools.texturepacker.TexturePacker.Settings;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		// Automatic texture packing
-		Settings settings = new Settings();
-		settings.paddingX = 2;
-	    settings.paddingY = 2;
-	    settings.minWidth = 32;
-	    settings.minHeight = 32;
-	    settings.maxHeight = 1024;
-	    settings.maxWidth = 1024;
-	    settings.stripWhitespaceX = true;
-	    settings.stripWhitespaceY = true;
-	    settings.filterMag = TextureFilter.Nearest;
-	    settings.filterMin = TextureFilter.Nearest;
-	    settings.flattenPaths = true;
-        TexturePacker.process(settings, "../graphics/png", "../android/assets", "game");
+//		Settings settings = new Settings();
+//		settings.paddingX = 2;
+//	    settings.paddingY = 2;
+//	    settings.minWidth = 32;
+//	    settings.minHeight = 32;
+//	    settings.maxHeight = 1024;
+//	    settings.maxWidth = 1024;
+//	    settings.stripWhitespaceX = true;
+//	    settings.stripWhitespaceY = true;
+//	    settings.filterMag = TextureFilter.Nearest;
+//	    settings.filterMin = TextureFilter.Nearest;
+//	    settings.flattenPaths = true;
+//        TexturePacker.process(settings, "../graphics/png", "../android/assets", "game");
 
 		
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
